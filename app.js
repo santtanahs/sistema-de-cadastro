fff6bb97ffe10db4c55d5e0dfd233c6a179c8dbe
santtanahs <hsanttana@outlook.com>
2022-12-01 14:45:26 -0300

    adicionando um botão e dando uma função a ele

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("O Paulo fez esse commit sem você ver na aula!")
 console.log("Enviando para o GitHub")
+
+    let segredo = 5;
+    let entrada = document.querySelector("input");
+
+function verifica() {
+    if(entrada.value == segredo) {
+        alert("Você ACERTOU!!!");
+    } else {
+        alert("Você ERROU!!!");
+    }
+}
+
+    let botao = document.querySelector("button");
+    botao.onclick = verifica;
+
+alert("Olá mundo");
