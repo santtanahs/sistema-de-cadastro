89d39222f4ecbbc3c7f78db99ca7a5dddeaa00e7
santtanahs <hsanttana@outlook.com>
2022-12-06 21:10:06 -0300

    adicionando um jogo de adivinhacao

--- app.js
+++ app.js
@@ -6,7 +6,79 @@ console.log("O Paulo fez esse commit sem você ver na aula!")
 console.log("Enviando para o GitHub")
 
 // teste de botão e caixa de texto 
-    let segredo = 5;
+    function sorteia () {
+        return Math.round(Math.random() * 10);
+    }
+    function sorteiaNumeros(quantidade) {
+
+        var segredos = [];
+
+        var numero = 1;
+
+        while (nuemro <= quantidade) {
+
+            var numeroAleatorio = sorteia();
+
+            if(numeroAleatorio !== 0) {
+                var achou = false;
+
+                for (var posicao = 0; posicao < segredos.length; posicao++) {
+                    if(segredos[posicao] == numeroAleatorio) {
+                        achou = true;
+                        break;
+                    }
+                }
+
+                if(achou == false) {
+                    segredos.push(numeroAleatorio);
+                    numero++;
+                }
+            }
+        }
+
+        return segredos;
+
+    }
+
+    var segredos = sorteiaNumeros(3);
+
+    var entrada = document.querySelector("input");
+    entrada.focus();
+
+    function verifica() {
+
+		var achou = false;
+
+		for(var posicao = 0; posicao < segredos.length; posicao++) {
+
+			if(entrada.value == segredos [posicao] ) {
+
+				alert("Você ACERTOU!!!");
+				achou = true;
+				break;
+			} 
+
+		}
+
+		if(achou == false) {
+
+			alert("Você ERROU!!!");
+		}
+		
+
+			entrada.value = "";
+			entrada.focus();
+	
+	}
+
+	var botao = document.querySelector("button");
+
+	botao.onclick = verifica;
+    
+    //console.log() serve para verificar as respostas
+	//console.log(segredos);
+
+    /*let segredo = 5;
     let entrada = document.querySelector("input");
 
 function verifica() {
@@ -20,4 +92,4 @@ function verifica() {
     let botao = document.querySelector("button");
     botao.onclick = verifica;
 
-alert("Consegue adivinhar em qual númemro estou pensando?");
+alert("Consegue adivinhar em qual númemro estou pensando?");*/
